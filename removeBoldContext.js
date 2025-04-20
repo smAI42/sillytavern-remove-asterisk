6882304a9e3a95678840f3430b4d6ff6f8fe4bdc
MAI <70988460+smAI42@users.noreply.github.com>
2025-04-20 23:46:36 +0100

    Update removeBoldContext.js

--- removeBoldContext.js
+++ removeBoldContext.js
@@ -1,9 +1,3 @@
-// ==UserScript==
-// @name         Remove Markdown Bold from Context
-// @description  Remove all **bold** from <context> content before sending to the model
-// @version      1.0
-// ==/UserScript==
-
 function removeBold(text) {
     // 匹配并保留中间内容：**xxx** -> xxx
     return text.replace(/\*\*(.*?)\*\*/g, '$1');
